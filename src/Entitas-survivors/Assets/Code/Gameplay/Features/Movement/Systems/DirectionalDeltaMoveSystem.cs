87c4fe17203c26d119a8cc949f2974d2dc3a6b5f
firstvf <101809257+firstvf@users.noreply.github.com>
2024-10-04 12:30:29 +0300

    move system
    - upd

--- src/Entitas-survivors/Assets/Code/Gameplay/Features/Movement/Systems/DirectionalDeltaMoveSystem.cs
+++ src/Entitas-survivors/Assets/Code/Gameplay/Features/Movement/Systems/DirectionalDeltaMoveSystem.cs
@@ -1,19 +1,20 @@
-﻿using Entitas;
+﻿using Code.Gameplay.Common.Time;
+using Entitas;
 
 namespace Assets.Code.Gameplay.Features.Movement.Systems
 {
     public class DirectionalDeltaMoveSystem : IExecuteSystem
     {
         private readonly IGroup<GameEntity> _movers;
+        private readonly ITimeService _timeService;
 
-        public DirectionalDeltaMoveSystem(GameContext context)
+        public DirectionalDeltaMoveSystem(GameContext context, ITimeService timeService)
         {
             _movers = context.GetGroup(GameMatcher
                 .AllOf(
                 GameMatcher.WorldPosition,
-                GameMatcher.Speed));            
-            
-            11:30 // 03 first system
+                GameMatcher.Speed));
+            _timeService = timeService;
         }
 
         public void Execute()
